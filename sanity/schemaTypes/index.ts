a281c9edddfcdc663eefb46fc60b481e3ca70646
bjamyl <alhassanjamil0@gmail.com>
2025-08-10 19:18:44 +0000

    set up sanity schemas

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,10 +1,25 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
 
-import {blockContentType} from './blockContentType'
-import {categoryType} from './categoryType'
-import {postType} from './postType'
-import {authorType} from './authorType'
+import { blockContentType } from "./blockContentType";
+import { categoryType } from "./categoryType";
+import { postType } from "./postType";
+import { authorType } from "./authorType";
+import { heroSectionType } from "./heroSection";
+import { serviceType } from "./service";
+import { testimonialType } from "./testimonial";
+import { faqType } from "./faq";
+import { teamMemberType } from "./teamMember";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType],
-}
+  types: [
+    blockContentType,
+    categoryType,
+    postType,
+    authorType,
+    heroSectionType,
+    serviceType,
+    testimonialType,
+    faqType,
+    teamMemberType,
+  ],
+};
