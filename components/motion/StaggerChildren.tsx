7851cd3861b52abb89e72d13feef9b1bf00b0910
evans-kwofie <evans.kwofie@vwaza.com>
2026-05-02 20:03:01 +0000

    fix:build error

--- components/motion/StaggerChildren.tsx
+++ components/motion/StaggerChildren.tsx
@@ -13,7 +13,7 @@ const itemVariants = {
   visible: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.75, ease: [0.21, 0.47, 0.32, 0.98] },
+    transition: { duration: 0.75, ease: [0.21, 0.47, 0.32, 0.98] as const },
   },
 }
 
